deddfcb4a9b059c6c4489f4aaaa001f2e011c931
Artur Jarosz <azjarosz@gmail.com>
2025-12-15 19:20:57 +0100

    FMB-9 Add basic theme setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {providePrimeNG} from "primeng/config"
+import Aura from '@primeuix/themes/aura';
+import {IconField, IconFieldModule} from "primeng/iconfield";
+import {InputIcon, InputIconModule} from "primeng/inputicon";
+import {FormsModule} from "@angular/forms";
+import {Avatar, AvatarModule} from "primeng/avatar";
+import {CommonModule} from '@angular/common';
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        IconField,
+        InputIcon,
+        FormsModule,
+        Avatar,
+        CommonModule,
+        AvatarModule, ButtonModule, IconFieldModule, InputIconModule, InputTextModule
+    ],
+    providers: [
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                    darkModeSelector: false
+                }
+            }
+        })
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/models/models.ts
@@ -0,0 +1,42 @@
+export enum TransactionType {
+    INCOME = 'INCOME',
+    EXPENSE = 'EXPENSE'
+
+}
+
+export interface BankTransaction {
+    id: number;
+    transactionDate: string;
+    transactionType: TransactionType;
+    amount: number;
+    category?: string;
+    categoryOverridden?: boolean;
+    details?: string;
+    recipientName?: string;
+    senderName?: string;
+    senderAccount?: string;
+    recipientAccount?: string;
+    source: string;
+    transactionHash: string;
+
+}
+
+export interface CategorySummary {
+    total: number;
+    transactions: BankTransaction[];
+}
+
+export interface Summary {
+    [categoryName: string]: CategorySummary;
+}
+
+export interface AnalyzedStatement {
+    summary: Summary;
+    transactions: BankTransaction[];
+}
+
+export interface OverrideCategory {
+    category: string;
+}
+
+
